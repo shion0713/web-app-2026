ac2109d704bdf84a4c386f51d4a44fd899bedfe5
shion0713 <shion.k.13@icloud.com>
2026-05-17 21:05:24 +0900

    added text.txt

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const app = express();
 
-// ルート1：トップページ
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('トップページです');
 });
 
-// ルート2：自己紹介ページ
 app.get('/about', (req, res) => {
   res.send('自己紹介ページです');
 });
 
-// ルート3：現在時刻を返す
 app.get('/time', (req, res) => {
   const now = new Date().toLocaleString('ja-JP');
   res.send('現在時刻：' + now);
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "APIが動いています", status: "ok" });
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'ok', message: 'サーバーが動いています'});
+});
+
+const messages = [];
+
+app.get("/api/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.post("/api/messages", (req, res) => {
+  const { username, text } = req.body;
+  const newMessage = { id: messages.length + 1, username, text };
+  messages.push(newMessage);
+  res.json(newMessage);
 });
 
 app.listen(3000, () => {
